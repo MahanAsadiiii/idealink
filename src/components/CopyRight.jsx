e2f6dc30d5ec9e7e00542d610aef1273fc82eb3c
Mahan Asadi <mahan.asadiiii@gmail.com>
2023-08-21 18:03:02 +0330

    edit copyright

--- src/components/CopyRight.jsx
+++ src/components/CopyRight.jsx
@@ -7,7 +7,7 @@ function CopyRight() {
     <div className='flex items-center justify-between bg-[#333333] text-white py-2 px-3 md:px-10'>
 
       <img src={Logo2} height={50} width={80} alt="logo-sign" />
-      <h5 className='text-xs font-light '>© 2023 AI-bees. All rights reserved.</h5>
+      <h5 className='text-xs font-light '>© 2023 Mahan Asadi.</h5>
     </div>
 
   )
