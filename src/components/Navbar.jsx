6f846242b7eaa492fd765039b6f6674100a5f53b
Mahan Asadi <mahan.asadiiii@gmail.com>
2023-08-21 17:49:03 +0330

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Logo2,Logo } from '../utilities/Index'
+import { Logo } from '../utilities/Index'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
