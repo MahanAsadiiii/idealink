410db987606708bfdec289bbe8cd1a0af83b053d
Mahan Asadi <mahan.asadiiii@gmail.com>
2023-08-27 11:26:13 +0330

    edit logo

--- src/components/CopyRight.jsx
+++ src/components/CopyRight.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Logo2, Logo } from '../utilities/Index'
+import { Logo2 } from '../utilities/Index'
 
 function CopyRight() {
   return (
 
     <div className='flex items-center justify-between bg-[#333333] text-white py-2 px-3 md:px-10'>
 
-      <img src={Logo2} height={50} width={80} alt="logo-sign" />
+      <img src={Logo2} height={60} width={110} alt="logo-sign" />
       <h5 className='text-xs font-light '>© 2023 Mahan Asadi.</h5>
     </div>
 
